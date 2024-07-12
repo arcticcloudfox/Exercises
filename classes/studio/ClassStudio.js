f93cf7bbe7c77198de036f833795d498431decdd
arcticcloudfox <27johnson279@gmail.com>
2024-07-11 20:11:40 -0500

    Classes studio

--- classes/studio/ClassStudio.js
+++ classes/studio/ClassStudio.js
@@ -7,3 +7,53 @@
 
 
 //Part 4 - Use the methods to boost Glad Gator’s status to Reserve or higher. How many tests will it take to reach Reserve status? How many to reach Accepted? Remember, scores cannot exceed 100%.
+
+class CrewCandidate {
+    constructor(name, mass, scores) {
+        this.name = name;
+        this.mass = mass;
+        this.scores = scores;
+    }
+    addScore(newScore) {
+        this.scores.push(newScore);
+    }
+    average() {
+        let scoreAverage = 0;
+        for (let i = 0; i < this.scores.length; i++) {
+            scoreAverage += this.scores[i]
+        }
+        return ((scoreAverage / this.scores.length).toFixed(1));
+    }
+    status() {
+        if (this.average() >= 90) {
+            return 'Accepted';
+        } else if (this.average() >= 80 && this.average() <= 89) {
+            return 'Reserve';
+        } else if (this.average() >= 70 && this.average() <= 79) {
+            return 'Probationary';
+        } else {
+            return 'Rejected';
+        }
+    }
+}
+
+const bear = new CrewCandidate('Bubba', 135, [88, 85, 90]);
+const maltese = new CrewCandidate('Merry', 1.5, [93, 88, 97]);
+const gator = new CrewCandidate('Glad', 225, [75, 78, 62]);
+
+console.log(bear);
+console.log(maltese);
+console.log(gator);
+
+bear.addScore(83);
+console.log(bear);
+
+console.log(`${bear.name} earned an average test score of ${bear.average()}% and has a status of ${bear.status()}.`);
+console.log(`${maltese.name} earned an average test score of ${maltese.average()}% and has a status of ${maltese.status()}.`);
+console.log(`${gator.name} earned an average test score of ${gator.average()}% and has a status of ${gator.status()}.`);
+
+//To get to reserve it takes adding at least 2 scores
+gator.addScore(98);
+gator.addScore(87);
+console.log(gator.average());
+console.log(gator.status());
